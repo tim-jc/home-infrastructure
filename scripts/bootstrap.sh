d2ba2855554d535225ab5f951784e02afeb9b950
tim-jc <tim_cross@me.com>
2026-07-31 16:05:51 +0100

    de-duplicated

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,10 +5,10 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 for step in \
     10-system-update.sh \
-    20-install-packages.sh \
-    30-install-docker.sh \
-    40-configure-host.sh \
-    50-create-directories.sh
+    20-install-packages.sh
+    #30-install-docker.sh \
+    #40-configure-host.sh \
+    #50-create-directories.sh
 do
     echo
     echo "==> Running ${step}"
